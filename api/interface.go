5a57df21270ba1ffdcb166f7aca04e1248f7fada
Artus Crusué Andermann <artus.andermann@neoway.com.br>
2024-03-27 19:33:39 -0300

    Update master

--- api/interface.go
+++
@@ -1,6 +0,0 @@
-package api
-
-type PhoneNumberUseCase interface {
-	CreatePhoneRecord(phoneNumber string) error
-	GetPhoneRecords() (map[string]interface{}, error)
-}
--- internal/repository/interface.go
+++
@@ -1,6 +0,0 @@
-package repository
-
-type MongoRepository interface {
-	StoragePhoneRecord(data map[string]interface{}, dbName string, collectionName string) error
-	GetPhoneRecords(dbName string, collectionName string) (results map[string]interface{}, err error)
-}
